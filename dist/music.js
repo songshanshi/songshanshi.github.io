5df0cc15606276de60d6b85f99f8c89d3f45f18b
songshanshi <1169494246@qq.com>
2019-07-29 19:14:54 +0800

    Site updated: 2019-07-29 19:14:50

---
+++ dist/music.js
@@ -0,0 +1,31 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+	{
+        name: "Solo",
+        artist: 'Clean Bandit&Demi Lovato',
+        url: 'http://www.ytmp3.cn/down/57696.mp3',
+        cover: 'http://p1.music.126.net/QGb9Vtyw7qHS00uEvPfM6g==/843325418547559.jpg?param=130y130',
+      },
+	  {
+        name: "Apologize",
+        artist: 'Martin Ermen',
+        url: 'http://pd2tflnys.bkt.clouddn.com/Martin%20Ermen%20-%20Apologize.mp3',
+        cover: 'http://p1.music.126.net/-_6mcI4VV5IKaiwhUAytbg==/1791104441647901.jpg?param=130y130',
+      },
+	  {
+        name: "River Flows in You",
+        artist: 'Yiruma',
+        url: 'http://pd2tflnys.bkt.clouddn.com/Yiruma%20-%20River%20Flows%20in%20You.flac',
+        cover: 'http://p1.music.126.net/8ZRSyI0ZN_4ah8uzsNd1mA==/2324367581169008.jpg?param=130y130',
+      },
+      {
+        name: '来自天堂的魔鬼',
+        artist: 'G.E.M.邓紫棋',
+        url: 'http://www.ytmp3.cn/down/73773.mp3',
+        cover: 'http://p1.music.126.net/5MmXpaP9r88tNzExPGMI8Q==/109951163370350985.jpg?param=130y130',
+      }
+    ]
+});
